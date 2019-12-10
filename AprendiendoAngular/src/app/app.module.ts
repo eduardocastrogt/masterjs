465355f2abfe11779fded32eec0009a150e6d977
Eduardo Castro <eduardocastrogt@gmail.com>
2019-12-09 23:21:20 -0600

    Routing
    Se crearon las rutas de navegación para los disintos componentes.

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { routing, appRoutingProviders } from './app.routing';
+import { ErrorComponent } from './components/error/error.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,12 +25,18 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    FormularioComponent,
+    PaginaComponent,
+    BlogComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ AprendiendoAngular/src/app/app.routing.ts
@@ -0,0 +1,28 @@
+//Importar los modulos del router de Angular
+
+import { ModuleWithProviders } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+//Importar los componentes a los que se les hará una pagina
+
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
+
+//Array de rutas
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'formulario', component: FormularioComponent},
+    {path: 'peliculas', component: PeliculasComponent},
+    {path: 'pagina-de-pruebas', component: PaginaComponent},
+    {path: '**', component: ErrorComponent}
+];
+
+//Exportar el modulo de rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
